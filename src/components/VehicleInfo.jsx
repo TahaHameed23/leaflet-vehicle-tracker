6aac0349251150cd77ea813a7919db4a5c2365e1
TahaHameed23 <89855847+TahaHameed23@users.noreply.github.com>
2024-09-25 17:42:21 +0530

    add:loading icon

--- src/components/VehicleInfo.jsx
+++ src/components/VehicleInfo.jsx
@@ -1,4 +1,12 @@
-const VehicleInfo = ({ position, heading, speed, time, start, isStarted}) => {
+import { useEffect } from "react";
+
+const VehicleInfo = ({ position, heading, speed, time, start, isStarted }) => {
+
+useEffect(() => {
+  const loadingIcon = document.getElementById('loading-icon');
+  isStarted ? loadingIcon.classList.remove('hidden') : loadingIcon.classList.add('hidden');
+}, [isStarted]);
+
   return (
     <div className="flex flex-col justify-between bg-transparent opacity-75 hover:opacity-100 border-blue-400 border-2 hover:border-blue-600 duration-200 rounded-lg max-w-lg mx-auto">
       <div className="p-8 rounded-md px-10">
@@ -9,14 +17,17 @@ const VehicleInfo = ({ position, heading, speed, time, start, isStarted}) => {
         <p className="text-gray-700"><strong>Speed:</strong> {(speed * 3.6).toFixed(2)} km/h</p>
         <p className="text-gray-700"><strong>Time:</strong> {time.toLocaleTimeString()}</p> 
       </div>
+
       <div className="mx-auto my-4">
-        <button   className={`bg-blue-600 px-20 rounded-md
-    ${isStarted ? 'bg-gray-400 cursor-not-allowed' : 'hover:bg-blue-500 active:bg-blue-700 duration-200 cursor-pointer '}`}
-        disabled={isStarted} 
-         onClick={()=>{
-          start()
-        }}
-        >Start</button>
+        <button
+          className={`bg-blue-600 px-20 rounded-md
+            ${isStarted ? 'bg-gray-400 cursor-not-allowed' : 'hover:bg-blue-500 active:bg-blue-700 duration-200 cursor-pointer'}`}
+          disabled={isStarted}
+          onClick={start}
+        >
+          Start
+          <span id="loading-icon" className="ml-1 hidden">🔄️</span>
+        </button>
       </div>
     </div>
   );
